48efafacfdea3ee5925653a3269a74544471b12f
Dexterski007 <mbouj007@gmail.com>
2023-10-05 09:49:22 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * this is the main function
+ * I should use printf only
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte\(s\)");
+	printf("Size of an int: 4 byte\(s\)");
+	printf("Size of a long int: 4 byte\(s\)");
+	printf("Size of a long long int: 8 byte\(s\)");
+	printf("Size of a float: 4 byte\(s\)");
+
+	return (0);
+}
