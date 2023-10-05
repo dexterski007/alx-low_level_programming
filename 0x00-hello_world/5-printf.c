f847ed907909e47e0bc6fbc846cfc8d5aa4b236b
Dexterski007 <mbouj007@gmail.com>
2023-10-05 09:40:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * this is the main function
+ * I should use printf only
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str1);
+
+	return (0);
+}
