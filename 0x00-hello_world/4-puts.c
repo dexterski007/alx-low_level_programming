d81eaec39f8f02be90d186e1ca9fd20522a6e26d
Dexterski007 <mbouj007@gmail.com>
2023-10-05 09:29:27 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ * this is the main function
+ * The code author is not responsible for any misuse of this function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(str1);
+
+	return (0);
+}
