e7b58327ca4d839803d24cdea47d2f8b52c3ce56
Dexterski007 <mbouj007@gmail.com>
2023-10-06 12:19:09 -0700

    hey

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,31 +13,22 @@ int main(void)
 {
 	int car;
 	int car2;
-	int car3;
-	int car4;
 
-	for (car = 0; car < 10; car++)
+	for (car = 0; car < 99; car++)
 	{
-		for (car2 = 0 ; car2 < 10; car2++)
+		for (car2 = car + 1 ; car2 < 99; car2++)
 		{
-			for (car3 = 0 ; car3 < 10; car3++)
-			{
-				for (car4 = 0 ; car4 < 10; car4++)
-					if (car3 != car || car4 != car2)
-					{
-						putchar(car + '0');
-						putchar(car2 + '0');
-						putchar(' ');
-						putchar(car3 + '0');
-						putchar(car4 + '0');
+					putchar((car / 10) + '0');
+					putchar((car % 10) + '0');
+					putchar(' ');
+					putchar((car2 / 10) + '0');
+					putchar((car2 % 10) + '0');
 
-		if (!(car == 9 && car2 == 8 && car3 == 9 && car4 == 8))
+		if (car != 98 || car2 != 99)
 		{
 		putchar(',');
 		putchar(' ');
-	}
-	}
-	}
+		}
 	}
 	}
 
