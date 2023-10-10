8653699db4afc239d0cc81c82e3044fb3f26d454
Dexterski007 <mbouj007@gmail.com>
2023-10-10 15:54:51 -0700

    modulo

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the sign of a number
+ * @n: The number to check
+ *
+ * Description:
+ * This function checks the sign of the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+int print_last_digit(int n)
+{
+	n = (n % 10);
+	return (n);
+}
