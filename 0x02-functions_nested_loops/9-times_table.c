3b10a3b976710eb7ba345f25b668edb47d9d636d
Dexterski007 <mbouj007@gmail.com>
2023-10-10 16:57:38 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the sign of a number
+ *
+ * Description:
+ * This function checks the sign of the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+void times_table(void)
+{
+	int t, f;
+
+	for (f = 0; f <= 9; f++)
+	{
+	for (t = 0; t <= 9; t++)
+	{
+		_putchar((t * f) + '0');
+		_putchar(',');
+		if (t < 9)
+		{
+		_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	}
+}
