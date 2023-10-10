5a70ac3c82dd86d4d66d794d86e07d6a2a3b0a21
Dexterski007 <mbouj007@gmail.com>
2023-10-10 15:08:33 -0700

    infinite

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,10 +16,10 @@ void print_alphabet_x10(void)
 
 	while (times < 10)
 	{
-	for (lett = 'a'; lett <= 'z' ; lett++)
+	for (lett = 'a'; lett <= 'z' ; lett++) {
 	_putchar(lett);
-
+	}
 	_putchar('\n');
-}
 times++;
+}
 }
