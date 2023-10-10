61eac57a7c3db22085f513f114102619c68baaed
Dexterski007 <mbouj007@gmail.com>
2023-10-10 16:29:31 -0700

    bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the sign of a number
+ *
+ * Description:
+ * This function checks the sign of the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+void jack_bauer(void)
+{
+	for (int h = 0, h <= 2, h++)
+	{
+		for (int h2 = 0, h2 <= 9 , h2++)
+		if ((h <= 1 && h2 <= 9) || (h <= 2 && h2 <= 3))
+		{
+			for (int m = 0, m <= 5, m++)
+			{
+				for (int m2 = 0, m2 <= 9, m2++)
+				{
+		_putchar(h + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+}
+}
+}
+}
+}
