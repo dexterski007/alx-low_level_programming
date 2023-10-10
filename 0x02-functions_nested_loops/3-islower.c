63335dbd52d2fe4a8d16f4bdb56f827c7c966244
Dexterski007 <mbouj007@gmail.com>
2023-10-10 15:23:57 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - functions and nested
+ *
+ * @c: this parameter is ok
+ *
+ * Description - functionsalphabet is lower parameter 'c' is actually cool
+ *
+ * Return: always 0 or 1
+ *
+ *
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97 && <= 122)
+		return (1);
+	else
+		return (0);
+}
