0162c75be07926ca2bf9f6d80e5ef9c6a34c7668
Dexterski007 <mbouj007@gmail.com>
2023-10-10 15:01:27 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - functions and nested
+ *
+ * Description - functionsalphabet
+ *
+ * Return: always 0
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char lett;
+	int times = 0;
+
+	while (times < 10)
+	{
+	for (lett = 'a'; lett <= 'z' ; lett++)
+	_putchar(lett);
+
+	_putchar('\n');
+}
+}
