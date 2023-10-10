86d52aaf112fdf5c7fa6a1b30e7d58caec89c62d
Dexterski007 <mbouj007@gmail.com>
2023-10-10 15:51:12 -0700

    zero

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _abs - prints the sign of a number
+ * @n: The number to check
+ *
+ * Description:
+ * This function checks the sign of the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = (n * -1)
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
