90a7dba47d2ad4032037870bc7d80d3b26a4a308
Dexterski007 <mbouj007@gmail.com>
2023-10-10 14:19:15 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.c"
+
+/**
+ * main - functions and nested
+ *
+ * Description - functionsalphabet
+ *
+ * Return: always 0
+ *
+ *
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 26 ; a++) {
+		print_alphabet();
+		 }
+
+	return (0);
+}
