c8c8bf1263ef8330189f845dc9b542ac5c3e0ff5
Dexterski007 <mbouj007@gmail.com>
2023-10-10 01:47:23 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <putchar.h>
+
+/**
+ * main - functions and nested
+ *
+ * Description - functions hell
+ *
+ * Return: always 0
+ *
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('_');
+	return (0);
+}
