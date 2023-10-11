ce1bf6f737b78695c56def3c0bfa53ae4d4342ff
Dexterski007 <mbouj007@gmail.com>
2023-10-10 18:43:33 -0700

    names

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the sign of a number
+ *
+ * @n: name
+ * Description:and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+void print_times_table(int n)
+{
+	int t, f, r;
+
+	if (n > 0 && n < 15)
+	{
+	for (f = 0; f <= n; f++)
+	{
+		_putchar('0');
+	for (t = 1; t <= n; t++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		r = t * f;
+		if (r < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar((r % 10) + '0');
+		}
+		else if (r < 100)
+		{
+			_putchar(' ');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+		else
+			_putchar((r / 100) + '0');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+	_putchar('\n');
+}
+}
+}
