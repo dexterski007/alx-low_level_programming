a3923d7f41b34e141c55b61a729473f501310064
Dexterski007 <mbouj007@gmail.com>
2023-10-10 18:23:25 -0700

    please

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,15 +16,13 @@
 
 void print_to_98(int n)
 {
-	for (n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n < 98)
+		while (n < 98)
 		{
 		printf("%d, ", n);
+		n++;
 		}
-		else
-		{
-			printf("%d", n);
+		printf("%d", n);
 		}
-	}
 }
