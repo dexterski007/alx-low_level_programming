d868011e51eee241c4401a538338da580cea097c
Dexterski007 <mbouj007@gmail.com>
2023-10-10 17:50:56 -0700

    easy?

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int_add - prints the sign of a number
+ *
+ * Description:
+ * This function checks the sign of the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+int int_add(int, int)
+{
+	int a, b, c;
+	
+	c = a + b;
+	       return(c);	
+}
