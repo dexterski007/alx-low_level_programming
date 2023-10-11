cbb2e9cfbbb608740ebd40c07e1f0fe3377d3fb8
Dexterski007 <mbouj007@gmail.com>
2023-10-10 18:14:57 -0700

    popoh

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints the sign of a number
+ *
+ * @n: hey jude
+ *
+ * Description: This function checks the sign of
+ * the number 'n' and prints the corresponding
+ * sign character. It returns 1 for positive numbers, -1 for negative numbers,
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+int print_to_98(int n)
+{
+	for (n <= 98)
+	{
+		if (n < 98)
+		{
+		printf("%d, ", n);
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+}
