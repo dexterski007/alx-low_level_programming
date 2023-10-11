a28c2cd09193c25a3007e9e64128899b12f8e6e5
Dexterski007 <mbouj007@gmail.com>
2023-10-10 19:17:03 -0700

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sign of a number
+ *
+ * Description: This function checks the sign of
+ * and 0 for zero.
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero.
+ */
+
+int main(void)
+{
+	int n, sum;
+
+	sum = 1;
+	n = 0;
+
+	while (n < 50)
+{
+	sum = sum + sum;
+	printf("%d, ", sum);
+	n++
+	}
+printf("\n");
+
+return (0);
+}
