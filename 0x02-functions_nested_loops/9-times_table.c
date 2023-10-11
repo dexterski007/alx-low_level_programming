238a652f02bae475e08a97be70213744c3d5864f
Dexterski007 <mbouj007@gmail.com>
2023-10-10 17:19:43 -0700

    comma

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,17 +23,17 @@ void times_table(void)
 		if (r < 10)
 		{
 		_putchar(r + '0');
+		_putchar(' ');
 		}
 		else
 		{
 		_putchar((r / 10) + '0');
 		_putchar((r % 10) + '0');
-		_putchar(',');
+		_putchar(' ');
 		}
-	}
 		if (t < 9)
 		{
-		_putchar(' ');
+		_putchar(',');
 		}
 	_putchar('\n');
 	}
