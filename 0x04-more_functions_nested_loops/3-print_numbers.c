6fa74263a35ed049ee6993dcc7afd526bc4844b5
Dexterski007 <mbouj007@gmail.com>
2023-10-12 11:27:53 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  print_numbers - checks if it is upper.
+ *
+ *
+ * Return: multi
+ *
+ */
+
+void print_numbers(void)
+{
+	int r = 0;
+	int g = 48;
+
+	while (r < 10)
+{
+	_putchar(g + r);
+	r++;
+}
+_putchar(10);
+}
