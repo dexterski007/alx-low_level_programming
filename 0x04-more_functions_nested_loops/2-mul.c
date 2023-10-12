1bfcd3adc5280cae24fa7db4417363c9721bcdad
Dexterski007 <mbouj007@gmail.com>
2023-10-12 10:57:02 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/**
+ * mul - checks if it is upper.
+ *
+ * @a: the letter is gotten from here
+ *
+ * @b: second param
+ *
+ * Return: multi
+ *
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
