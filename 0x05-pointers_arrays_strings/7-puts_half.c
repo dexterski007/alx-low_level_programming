ce71859aaad02b89d29c8ba69eecf9ef0eb247ac
Dexterski007 <mbouj007@gmail.com>
2023-10-16 13:40:25 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ *
+ * @str: yeah science
+ * Return: void.
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int b = 0;
+
+	while (str[a] != '\0')
+	{
+	a++;
+	}
+
+	if (a % 2 == 0)
+	{
+	b = a / 2;
+	while (str[b] != '\0')
+	{
+	_putchar(str[b]);
+	b++;
+	}
+	}
+	else
+	{
+	b = (a - 1) / 2;
+	while (str[b] != '\0')
+	{
+	_putchar('\n');
+	b++;
+	}
+	}
+	_putchar('\n');
+}
