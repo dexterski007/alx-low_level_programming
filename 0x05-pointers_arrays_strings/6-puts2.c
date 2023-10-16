ad36eee4ac81ae228e6868660f89180887ba9919
Dexterski007 <mbouj007@gmail.com>
2023-10-16 13:17:19 -0700

    loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,15 +10,16 @@
 void puts2(char *str)
 {
 	int a = 0;
+	int b = 0;
 
 	while (str[a] != '\0')
 	{
-	if (a % 2 == 0)
-	{
-	_putchar(str[a]);
 	a++;
 	}
-	a++;
+	while (b < a)
+	{
+	_putchar(str[b]);
+	b += 2;
 	}
 	_putchar('\n');
 }
