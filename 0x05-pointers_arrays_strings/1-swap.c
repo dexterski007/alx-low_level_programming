5eca540060d52e1c5eeffc64b11b004fcf44f4d5
Dexterski007 <mbouj007@gmail.com>
2023-10-16 07:33:20 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - check the code
+ *
+ * @a: yeah science
+ * @b: yeah science 2
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c, d;
+	c = &d;
+	*c = *b;
+	*b = *a;
+	*a = *c;
+}
