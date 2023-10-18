6f360c242c561ba0c3c51858b1a96da2b838d52e
Dexterski007 <mbouj007@gmail.com>
2023-10-18 15:40:46 -0700

    pfiou

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,25 +11,29 @@ void print_number(int n)
 
 {
 	int i = 0;
-	int str[20];
+	char str[20];
+	int m = n;
 
-	if (n < 0)
-	{
-	_putchar('-');
-	}
 	if (n == 0)
 	{
-	_putchar('0');
-	return;
+	str[i] = '0';
+	i++;
 	}
+	if (n < 0)
+	n = -n;
 	while (n > 0)
 	{
-	str[i] = n % 10;
+	str[i] = (n % 10) + '0';
 	n /= 10;
+	i++;
+	}
+	if (m < 0)
+	{
+	str[i] = '-';
 	i++;
 	}
 	for (i--; i >= 0; i--)
 	{
-	_putchar(str[i] + '0');
+	_putchar(str[i]);
 	}
 }
