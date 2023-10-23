83854db3a2dbc3efa18f91dcda25eda65ed453a3
Dexterski007 <mbouj007@gmail.com>
2023-10-23 01:40:58 -0700

    without_check

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - sets memory
+ *
+ * @s: pointer
+ * @b: byte
+ * @n: number
+ *
+ * Return: char
+**/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = _putchar(b);
+	}
+	return (s);
+}
