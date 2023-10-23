a6bbc00ca9abdeed36ca3dace8f97bc2feeecc01
Dexterski007 <mbouj007@gmail.com>
2023-10-23 11:35:19 -0700

    if

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,12 +19,13 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		f = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 			c++;
 			f = 1;
+			}
 		}
 		if (f == 0)
 		return (f);
