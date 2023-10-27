f2e6b7d8e57542a701c6ec27b618c75fceca81dd
Dexterski007 <mbouj007@gmail.com>
2023-10-27 11:31:14 -0700

    yeah

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  coin change
+ *
+ * @argc: count
+ * @argv: text
+ *
+ * Return: 0 always
+**/
+
+int main(int argc, char *argv[])
+
+{
+	int coins = 0, change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	change = atoi(argv[1]);
+	while (change > 0)
+	{
+		coins++;
+		if (change - 25 >= 0)
+		{
+		change -= 25;
+		continue;
+		}
+		if (change - 10 >= 0)
+		{
+		change -= 10;
+		continue;
+		}
+		if (change - 5 >= 0)
+		{
+		change -= 5;
+		continue;
+		}
+		if (change - 2 >= 0)
+		{
+		change -= 2;
+		continue;
+		}
+		if (change - 1 >= 0)
+		{
+		change -= 1;
+		continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
