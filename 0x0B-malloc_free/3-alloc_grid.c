4dca5974a0ae6609de10dce8e258f916407ac75c
Dexterski007 <mbouj007@gmail.com>
2023-10-30 08:40:53 -0700

    free2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,14 +28,8 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
-	{
 	tab[i] = (int *)malloc(width * sizeof(int));
-	{
-	if (tab[i] == NULL)
-	free(tab);
-	return (NULL);
-	}
-	}
+
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
