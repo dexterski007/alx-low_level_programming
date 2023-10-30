25a1904e476d935ba5e9ef5f656e15451d4d15dd
Dexterski007 <mbouj007@gmail.com>
2023-10-30 08:36:02 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - is to concatenate
+ *
+ * @width: width
+ * @height: height
+ *
+ * Return: integer
+**/
+
+int **alloc_grid(int width, int height)
+
+{
+	int **tab;
+	int i = 0, j = 0;
+
+	if (width <= 0)
+	return (NULL);
+	if (height <= 0)
+	return (NULL);
+
+	tab = (int **)malloc(height * sizeof(int *));
+	if (tab == NULL)
+	return (NULL);
+
+	for (i = 0; i < height; i++)
+	tab[i] = (int *)malloc(width * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		tab[i][j] = 0;
+	}
+
+	return (tab);
+}
