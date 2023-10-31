957910f96c4635ce3678efe414e1904f8611d209
Dexterski007 <mbouj007@gmail.com>
2023-10-31 12:54:28 -0700

    yeah_science

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,22 +3,21 @@
 #include <string.h>
 
 /**
- * strtow - is to split strings
+ * counter - to count words
  *
  * @str: string
  *
- * Return: char
+ * Return: integer
 **/
 
-char **strtow(char *str)
+
+
+int counter(char *str)
 
 {
-	char **tab;
-	int i = 0, j = 0, d = 0, f = 0;
+	int d  = 0, i = 0, j = 0;
 	int len = strlen(str);
 
-	if (strcmp(str, "") == 0 || str == NULL)
-	return (NULL);
 
 	for (i = 0; i < len; i++)
 	{
@@ -33,12 +32,33 @@ char **strtow(char *str)
 		d++;
 		}
 	}
+	return (d);
+}
 
+
+/**
+ * strtow - is to split strings
+ *
+ * @str: string
+ *
+ * Return: char
+**/
+
+
+char **strtow(char *str)
+{
+	char **tab;
+	int i = 0, j = 0, d = 0, f = 0;
+	int len = strlen(str);
+
+	if (strcmp(str, "") == 0 || str == NULL)
+	return (NULL);
+
+	d = counter(str);
 	tab = (char **)malloc((d + 1) * sizeof(char *));
 	if (tab == NULL)
 	return (NULL);
 	d = 0;
-
 	for (i = 0; i < len; i++)
 	{
 		if (str[i] != ' ')
@@ -49,7 +69,6 @@ char **strtow(char *str)
 		i++;
 		j++;
 		}
-	
 			tab[d] = (char *)malloc(j + 1);
 			if (tab[d] == NULL)
 			return (NULL);
