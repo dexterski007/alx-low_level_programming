b9b19020ae87b4ac31805aa60d4bd7d2eddca78e
Dexterski007 <mbouj007@gmail.com>
2023-11-04 08:27:25 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ *
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: this structure contais dog info
+**/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
