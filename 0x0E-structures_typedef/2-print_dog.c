ef9d0c15ff999ee58212ddeca9d2d1a787ab97c1
Dexterski007 <mbouj007@gmail.com>
2023-11-04 09:26:32 -0700

    age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,7 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name);
 	if (d->name == NULL)
 	printf("Name: (nil)\n");
-	if (d->age != 0)
 	printf("Age: %f\n", d->age);
-	if (d->age == 0)
-	printf("Age: (nil)\n");
 	if (d->owner != NULL)
 	printf("Owner: %s\n", d->owner);
 	if (d->owner == NULL)
