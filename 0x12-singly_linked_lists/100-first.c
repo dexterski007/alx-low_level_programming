dc5410615c12516e1c1743c4187d7c131bacb6e5
Dexterski007 <mbouj007@gmail.com>
2023-11-16 13:06:14 -0800

    tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+void tortoise(void) __attribute__((constructor));
+#include <stdio.h>
+/**
+ * tortoise - before rabbit
+ */
+
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
