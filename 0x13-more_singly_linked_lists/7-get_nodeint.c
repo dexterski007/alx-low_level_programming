79c1e1eb2b2828e1b73d8618b835c26f51f8d5dd
Dexterski007 <mbouj007@gmail.com>
2023-11-21 12:09:16 -0800

    oo

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return node at index
+ *
+ * @head: node pointer
+ *
+ * Return: data of node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL && i < index)
+	{
+		head = head->next;
+		i++;
+	}
+
+	if (i == index)
+		return (head);
+	else
+		return (NULL);
+}
