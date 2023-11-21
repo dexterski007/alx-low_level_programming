02b0a7a2e4a0c7aa335e3c960460faa95aea442b
Dexterski007 <mbouj007@gmail.com>
2023-11-21 11:28:06 -0800

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free nodes
+ *
+ * @head: node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	while (*head)
+	{
+		curr = (*head)->next;
+		free(*head);
+		*head = curr;
+	}
+	*head = NULL;
+}
