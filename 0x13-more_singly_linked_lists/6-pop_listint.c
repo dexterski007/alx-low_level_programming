c1dc90f167c107f0923807b303b2d9f795a14a4d
Dexterski007 <mbouj007@gmail.com>
2023-11-21 12:00:23 -0800

    valg

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - free nodes
+ *
+ * @head: node pointer
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *curr = *head;
+	int n = 0;
+
+	if (*head == NULL)
+		{
+			return (0);
+		}
+
+		n = (*head)->n;
+		*head = (*head)->next;
+		free (curr);
+	return (n);
+}
