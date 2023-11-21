093e9d95355109154da416adfbdfebb83151a6c7
Dexterski007 <mbouj007@gmail.com>
2023-11-21 12:32:44 -0800

    j

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - sum node at index
+ *
+ * @head: node pointer
+ *
+ * Return: data of node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *curr = *head;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || head == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	for (count = 0; curr && count < idx; count++)
+	{
+		if (count == idx - 1)
+		{
+			new_node->next = curr->next;
+			curr->next = new_node;
+			return(new_node);
+		}
+		else
+			curr = curr->next;
+	}
+
+	return (NULL);
+}
