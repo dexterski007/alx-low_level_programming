5d7ee3063970ccce1a099a5b982d571e8263c4c5
Dexterski007 <mbouj007@gmail.com>
2023-11-23 12:02:09 -0800

    biflip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - reverse bits
+ *
+ * @n: source
+ * @m: destination
+ *
+ * Return: num of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit = n ^ m;
+	unsigned int count = 0;
+
+	while (bit > 0)
+		 {
+		 	count += bit & 1;
+		 	bit >>= 1;
+		 }
+
+	return (count);
+}
