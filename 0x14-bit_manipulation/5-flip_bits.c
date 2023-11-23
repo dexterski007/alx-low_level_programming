9890ab5ac4add33b58dfc68bae5c342dffa095cb
Dexterski007 <mbouj007@gmail.com>
2023-11-23 12:03:08 -0800

    betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,10 +15,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 
 	while (bit > 0)
-		 {
-		 	count += bit & 1;
-		 	bit >>= 1;
-		 }
+	{
+	count += bit & 1;
+	bit >>= 1;
+	}
 
 	return (count);
 }
