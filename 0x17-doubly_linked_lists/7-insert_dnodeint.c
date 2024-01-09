c09a2e276f138b037afa8926e383e032abbe043d
Dexterski007 <mbouj007@gmail.com>
2024-01-09 10:14:41 -0800

    newno

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,38 +14,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new_node, *temp = *h;
 
-	if (*h == NULL && idx != 0)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-	while (i < idx - 1 && h != NULL)
+
+	for (i = 0; i < idx; i++)
 	{
 		temp = temp->next;
-		i++;
+		if (temp == NULL)
+			return (NULL);
 	}
 
-	if (temp == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
+	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
-
-	if (new_node->next == NULL)
-		new_node->next->prev = new_node;
+	temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
