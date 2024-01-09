3e2db9386f324a978367b3ab47dca8836ef503c5
Dexterski007 <mbouj007@gmail.com>
2024-01-09 13:04:08 -0800

    next

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp = *head;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -25,13 +25,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 1; i < index - 1 && temp != NULL; i++)
+	while(i < index && temp != NULL)
+	{
 		temp = temp->next;
+		i++;
+	}
 
-	if (temp == NULL || temp->next == NULL)
+	if (temp == NULL)
 		return (-1);
-
-	temp->next->prev = temp->prev;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
 	if (temp->prev != NULL)
 		temp->prev->next = temp->next;
 	free (temp);
