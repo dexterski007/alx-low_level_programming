de853068b1bc45245d9ecdfeccfbb2a019669606
Dexterski007 <mbouj007@gmail.com>
2024-01-09 10:22:02 -0800

    hey

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
-	temp->next->prev = new_node;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
