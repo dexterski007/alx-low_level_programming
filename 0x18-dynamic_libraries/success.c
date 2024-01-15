84900750bbd13c4c66655bf1d6056a4957d3848c
Dexterski007 <mbouj007@gmail.com>
2024-01-15 11:46:01 -0800

    success

--- 0x18-dynamic_libraries/success.c
+++ 0x18-dynamic_libraries/success.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void main()
-
+int printf(const char *format, ...)
 {
-	printf("9 8 10 24 75 9\n");
-	printf("Congratulations, you win the Jackpot!\n");
+	write(1, "9 8 10 24 75 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
