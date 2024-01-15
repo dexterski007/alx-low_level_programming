ae0c158a6dc9eecb330386cec965a34795e6dd84
Dexterski007 <mbouj007@gmail.com>
2024-01-15 11:51:22 -0800

    hey

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/dexterski007/alx-low_level_programming/raw/main/0x18-dynamic_libraries/success.so
+wget -P /tmp https://github.com/dexterski007/alx-low_level_programming/raw/main/0x18-dynamic_libraries/success.so
 export LD_PRELOAD=/tmp/success.so
