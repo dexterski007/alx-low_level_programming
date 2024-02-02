c2c58dddb3ad9ba4c0375846886e37da4b07f906
Dexterski007 <mbouj007@gmail.com>
2024-02-02 10:09:45 -0800

    hey

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,8 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *nod;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
@@ -21,13 +22,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	while (ht->array[i])
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			return (ht->array[i]->value);
-		}
-		i++;
-	}
-	return (NULL);
+	nod = ht->array[index];
+
+	while(nod && strcmp(nod->key, key) != 0)
+		nod = nod->next;
+
+	if (nod == NULL)
+		return (NULL);
+
+	return (nod->value);
 }
