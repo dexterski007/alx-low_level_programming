08ee2456dc510139e8e88b046c1cbb32b07589bf
Dexterski007 <mbouj007@gmail.com>
2024-02-02 01:18:00 -0800

    hey

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,13 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
-	if (size <= 0)
-		return (NULL);
-
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
+	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
 	{
@@ -29,7 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
-		new_table->size = size;
+		new_table->array[i] = NULL;
 
 	return (new_table);
 
