00ef7c0bc06a6dd67867ed132015161ab5cb4f69
Dexterski007 <mbouj007@gmail.com>
2024-02-02 07:15:23 -0800

    hey

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,12 +21,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	for (i = index; ht->array[i]; i++)
+	while (ht->array[i])
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			return (ht->array[i]->value);
 		}
+		i++;
 	}
 	return (NULL);
 }
