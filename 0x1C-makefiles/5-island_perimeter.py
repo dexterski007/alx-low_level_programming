a58baaa023ea85bac5e781ec8125a0e28825ac6d
Dexterski007 <mbouj007@gmail.com>
2024-03-20 15:39:44 -0700

    technical

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,2 +1,17 @@
-Lets makefiles
+#!/usr/bin/python3
+""" island is home """
 
+
+def island_perimeter(grid):
+    """ grid calculator """
+    perim = 0
+    for row in range(len(grid)):
+        for cell in range(len(grid[0])):
+            if grid[row][cell] == 1:
+                perim += 4
+                if row > 0 and grid[row - 1][cell] == 1:
+                    perim -= 2
+                if cell > 0 and grid[row][cell - 1] == 1:
+                    perim -= 2
+
+    return perim
